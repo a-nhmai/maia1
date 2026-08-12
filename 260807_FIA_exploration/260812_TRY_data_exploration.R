4fe2619be12c2be9d484e854862a6baeb7780d0a
a-nhmai <27maianh@gmail.com>
2026-08-12 14:40:52 -0500

    working on trait dists
    Merge branch 'master' of https://github.com/a-nhmai/mai_a

--- 260807_FIA_exploration/260812_TRY_data_exploration.R
+++ 260807_FIA_exploration/260812_TRY_data_exploration.R
@@ -4,11 +4,14 @@
 #12 August 2026
 
 #Loading in libraries
+    #install.packages("cowplot")
+
 lib_ls <- c("dplyr",
             "ggplot2",
             "forcats",
             "janitor",
-            "stringr")
+            "stringr",
+            "cowplot")
 
 sapply(lib_ls, library, character.only = TRUE)
 
@@ -17,7 +20,7 @@ sapply(lib_ls, library, character.only = TRUE)
 trait_df_raw <- read.csv("~/maia1/260807_FIA_exploration/try_angio_clean.csv")
 trait_df <- trait_df_raw |>
               select(DatasetID, 
-                     SpeciesName,
+                     AccSpeciesName,
                      ObservationID,
                      TraitID,
                      TraitName,
@@ -27,9 +30,73 @@ trait_df <- trait_df_raw |>
                      OrigUnitStr,
                      StdValue,
                      Comment) |>
-              rename(value = StdValue) |>
-              clean_names()
+              rename(value = StdValue,
+                     spp_name = AccSpeciesName) |>
+              clean_names() |> 
+              mutate(across(c(dataset_id:data_id),
+                            as.factor)) |>
+              mutate(across(c(orig_value_str, value),
+                            as.numeric))
+
+#Removed data and justification
+trait_df <- trait_df |>
+              filter(trait_name != "Leaf area index (LAI) of a single plant")
 
 
+#EDA
+
+    #how many species total are there?
+        unique(trait_df$spp_name)
+        #739 species
+
+
+    #how many oak species are there
+    trait_df |> 
+      count(spp_name) |>
+      arrange(desc(n)) |>
+      filter(str_detect(spp_name, "Quercus")) 
+      #40 Quercus species
+
+
+    #how many observations per trait are there?
+        trait_df |>
+          count(trait_name)
+        #Leaf area per... undefined is highly represented.
+        #Crown/canopy height has a decent amount of observations but nowhere near LMA
+        #Bark thickness and leaf lifespan/longevity have around 300 observations
+        
+        #Data cleaning note: LAI of a single plant = 1, was removed above
+
+    #what does the distribution of the traits look like across all species?
+        trait_df |>
+          filter(trait_name == "Leaf area per leaf dry mass (specific leaf area, SLA or 1/LMA): undefined if petiole is in- or excluded") |> 
+          ggplot(aes(x = value)) +
+            geom_histogram()
+        #very right skewed, logarithmic scale looks better and very normal
+        trait_df |>
+          filter(trait_name == "Leaf area per leaf dry mass (specific leaf area, SLA or 1/LMA): undefined if petiole is in- or excluded") |> 
+          ggplot(aes(x = log(value))) +
+          geom_histogram()
+
+        traits <- unique(trait_df$trait_name) |> as.vector()
+        
+        plot_vector <- list()
+        
+        plot_function <- function(data, value_list){
+          
+          list_ct <- length(value_list)
+          
+          for(i in 1:list_ct){
+            p <- data |>
+              filter(trait_name == traits[i]) |>
+              ggplot(aes(x = value_list)) +
+              geom_histogram()
+            
+            plot_vector[i] <- p
+          }
+          
+        }
+
+        plot_function(trait_df, traits)
 
 
