0d6bce50b490fa5810f475e99444bb4c296ea82a
Anh Mai <maimac@infra04-wg324.eduroam.wireless.umn.edu>
2026-08-12 17:05:56 -0500

    Finished the for loop for trait distribution analysis

--- 260807_FIA_exploration/260812_TRY_data_exploration.R
+++ 260807_FIA_exploration/260812_TRY_data_exploration.R
@@ -40,7 +40,8 @@ trait_df <- trait_df_raw |>
 
 #Removed data and justification
 trait_df <- trait_df |>
-              filter(trait_name != "Leaf area index (LAI) of a single plant")
+              filter(trait_name != "Leaf area index (LAI) of a single plant") |> #only has 1 value
+              filter(trait_name != "Plant lifespan (longevity)") #mostly NAs and 1 value, might have to double check this!!!
 
 
 #EDA
@@ -74,14 +75,17 @@ trait_df <- trait_df |>
             geom_histogram()
         #very right skewed, logarithmic scale looks better and very normal
         trait_df |>
-          filter(trait_name == "Leaf area per leaf dry mass (specific leaf area, SLA or 1/LMA): undefined if petiole is in- or excluded") |> 
-          ggplot(aes(x = log(value))) +
+          filter(trait_name == "Root length per root dry mass (specific root length, SRL)") |> 
+          ggplot(aes(x = value)) +
           geom_histogram()
 
+        
+        
+        #setup for forloop
         traits <- unique(trait_df$trait_name) |> as.vector()
+        plot_vector <- vector("list", length(traits))
         
-        plot_vector <- list()
-        
+        #for loop
         plot_function <- function(data, value_list){
           
           list_ct <- length(value_list)
@@ -89,14 +93,28 @@ trait_df <- trait_df |>
           for(i in 1:list_ct){
             p <- data |>
               filter(trait_name == traits[i]) |>
-              ggplot(aes(x = value_list)) +
+              ggplot(aes(x = value)) +
               geom_histogram()
             
-            plot_vector[i] <- p
+            plot_vector[[i]] <- p
           }
+          
+          plot_vector
           
         }
 
-        plot_function(trait_df, traits)
-
-
+        distrib_plots <- plot_function(trait_df, traits)
+      
+        plot_grid(distrib_plots[[1]],
+                  distrib_plots[[2]],
+                  distrib_plots[[3]],
+                  distrib_plots[[4]],
+                  distrib_plots[[5]],
+                  distrib_plots[[6]],
+                  labels = traits,
+                  label_size = 8,
+                  hjust = -0.3)
+            
+              #all traits right skewed, likely will need to log-transform most of them
+              #rooting length per dry mass/specific root length data seems a little sparse, so plot doesn't look great. possibly discrete values?
+        
