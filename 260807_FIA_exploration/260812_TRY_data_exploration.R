bf1f954f5311bfb7df903eac92a944bc72734028
a-nhmai <27maianh@gmail.com>
2026-08-13 15:24:04 -0500

    addition of vessel trait data and further cleaning

--- 260807_FIA_exploration/260812_TRY_data_exploration.R
+++ 260807_FIA_exploration/260812_TRY_data_exploration.R
@@ -18,6 +18,7 @@ sapply(lib_ls, library, character.only = TRUE)
 
 #Loading in df
 trait_df_raw <- read.csv("~/maia1/260807_FIA_exploration/try_angio_clean.csv")
+
 trait_df <- trait_df_raw |>
               select(DatasetID, 
                      AccSpeciesName,
@@ -29,6 +30,7 @@ trait_df <- trait_df_raw |>
                      OrigValueStr,
                      OrigUnitStr,
                      StdValue,
+                     UnitName,
                      Comment) |>
               rename(value = StdValue,
                      spp_name = AccSpeciesName) |>
@@ -37,6 +39,20 @@ trait_df <- trait_df_raw |>
                             as.factor)) |>
               mutate(across(c(orig_value_str, value),
                             as.numeric))
+
+#correction for vessel length issue
+vessel_l <- trait_df |>
+                filter(trait_name == "Stem conduit length") |>
+                mutate(orig_value_str = ifelse(orig_unit_str != "cm", 
+                                               orig_value_str/10000,
+                                               orig_value_str),
+                       orig_unit_str = "cm",
+                       unit_name = "cm (see comment)",
+                       value = orig_value_str,
+                       comment = paste(comment, ", was fixed by AM because TRY had only NAs")) 
+trait_df <- trait_df |>
+                filter(trait_name != "Stem conduit length") |> 
+                bind_rows(vessel_l)
 
 #Removed data and justification
 trait_df <- trait_df |>
@@ -48,7 +64,7 @@ trait_df <- trait_df |>
 
     #how many species total are there?
         unique(trait_df$spp_name)
-        #739 species
+        #780 species
 
 
     #how many oak species are there
@@ -56,17 +72,20 @@ trait_df <- trait_df |>
       count(spp_name) |>
       arrange(desc(n)) |>
       filter(str_detect(spp_name, "Quercus")) 
-      #40 Quercus species
+      #43 Quercus species
 
 
     #how many observations per trait are there?
         trait_df |>
-          count(trait_name)
+          count(trait_name) |>
+          arrange(desc(n))
         #Leaf area per... undefined is highly represented.
+        #Stem conduit diameter has a fair amount of observations and density has a decent amout
+        #whereas stem conduit length is much more underrepresented (only 40)
         #Crown/canopy height has a decent amount of observations but nowhere near LMA
         #Bark thickness and leaf lifespan/longevity have around 300 observations
-        
-        #Data cleaning note: LAI of a single plant = 1, was removed above
+        #Root length per dry root mass... n = 16
+  
 
     #what does the distribution of the traits look like across all species?
         trait_df |>
@@ -75,15 +94,17 @@ trait_df <- trait_df |>
             geom_histogram()
         #very right skewed, logarithmic scale looks better and very normal
         trait_df |>
-          filter(trait_name == "Root length per root dry mass (specific root length, SRL)") |> 
+          filter(trait_name == "Stem conduit length") |> 
           ggplot(aes(x = value)) +
           geom_histogram()
 
         
         
         #setup for forloop
-        traits <- unique(trait_df$trait_name) |> as.vector()
+        labels <- trait_df |> distinct(trait_name, unit_name)
+        traits <- labels["trait_name"] |> unlist() |> as.vector()
         plot_vector <- vector("list", length(traits))
+        units <- labels["unit_name"] |> unlist() |> as.vector()
         
         #for loop
         plot_function <- function(data, value_list){
@@ -111,9 +132,12 @@ trait_df <- trait_df |>
                   distrib_plots[[4]],
                   distrib_plots[[5]],
                   distrib_plots[[6]],
-                  labels = traits,
+                  distrib_plots[[7]],
+                  distrib_plots[[8]],
+                  distrib_plots[[9]],
+                  labels = paste(traits, "(", units, ")"),
                   label_size = 8,
-                  hjust = -0.3)
+                  hjust = -0.2)
             
               #all traits right skewed, likely will need to log-transform most of them
               #rooting length per dry mass/specific root length data seems a little sparse, so plot doesn't look great. possibly discrete values?
