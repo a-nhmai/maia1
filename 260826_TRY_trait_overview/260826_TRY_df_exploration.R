f4b1c70b7af486723b991adf8bdb17e434299af7
a-nhmai <27maianh@gmail.com>
2026-08-27 12:01:46 -0500

    final cleaning of the TRY data

--- 260826_TRY_trait_overview/260826_TRY_df_exploration.R
+++ 260826_TRY_trait_overview/260826_TRY_df_exploration.R
@@ -19,7 +19,7 @@ lib_ls <- c("dplyr",
 
 sapply(lib_ls, library, character.only = TRUE)
 
-#Data: loading and cleaning--------------------------------
+#Data: loading and initial cleaning--------------------------------
 trait_df_raw <- read.csv("~/maia1/260807_FIA_exploration/try_angio_clean.csv") 
   #this df was cleaned from maia1/260807_FIA_exploration/260812_TRY_cleaning.R
   #only angiosperms that are listed in REF_SPECIES.csv from the FIA database are included
@@ -47,7 +47,10 @@ trait_df <- trait_df_raw |>
                 mutate(across(c(dataset_id:data_id),
                               as.factor)) |>
                 mutate(across(c(orig_value_str, value),
-                              as.numeric))
+                              as.numeric)) |>
+                filter(trait_id != 926) #only one LAI of a single plant
+    #mostly selecting relevant columns, cleaning up names, and making sure 
+    #variables are the correct data type
 
 #Cleaning: overview--------------------------------
 glimpse(trait_df)
@@ -74,7 +77,7 @@ trait_df_dup |>
     summarize(ids = n_distinct(observation_id))
 
 trait_df_cl <- trait_df_dup |>
-                    filter(duplicated != TRUE)
+                    filter(duplicated == FALSE)
 
 
 #Cleaning: checking incongruence in original units and (new) units--------------------------------
@@ -84,7 +87,7 @@ trait_df_cl <- trait_df_dup |>
     #there should be an equal amount of single and double mismatches
     #if the difference between single and double is negative, then there are too many 
 
-#double mismatch
+#Cleaning: ID'ing incongruence of mismatches--------------------------------
 mism_d <- trait_df_cl |>
         mutate(unit_mismatch = ifelse(orig_unit_str != unit_name, "TRUE", "FALSE"),
                value_mismatch = ifelse(orig_value_str != value, "TRUE", "FALSE"),
@@ -108,19 +111,52 @@ bind_cols(mism_s, mism_d[2]) |>
   mutate(diff = mismatches_s - mismatches_d,
          issue = ifelse(diff != 0, "TRUE", "FALSE"))
 
+#Cleaning: filtering incongruence of mismatches--------------------------------
 
+single_mismatches <- trait_df_cl |>
+                         mutate(unit_mismatch = ifelse(orig_unit_str != unit_name, "TRUE", "FALSE"),
+                         value_mismatch = ifelse(orig_value_str != value, "TRUE", "FALSE"),
+                         single_mismatch = ifelse(unit_mismatch != unit_name & orig_value_str == value, 
+                                                  "TRUE", 
+                                                  "FALSE"),
+                         across(unit_mismatch:single_mismatch, as.logical))
+
+trait_mism <- trait_df_cl |>
+                  bind_cols(single_mismatches$single_mismatch) |>
+                  rename("mism_s" = "...16")
+
+trait_mism |>
+  filter(mism_s == TRUE) |>
+  filter(orig_value_str == value & unit_name != orig_unit_str)
+
+    #The mismatches are ok. Mostly months -> month and m2/g -> mm2 mg-1; i.e. one to one conversions
 
 #CSV writing: saving the cleaned df without duplicates in desired format --------------------------------
 
-trait_df_dup |> 
-  filter(trait_id != 926) |> #filtering out Leaf area index (LAI) of a single plant because only 1 observation
-  slice_sample(n = 5)
+cleaned_trait_df <- trait_df_cl |>
+                        select(-duplicated, -orig_unit_str, -origl_name, -orig_value_str) |>
+                        mutate(id_dfdo = str_c(dataset_id, "_" , data_id, "_", observation_id), #unique identifier for a specific DataFrame, Data, and Obs id
+                               source = str_c("TRY.", dataset), #adding that the source is try data
+                               comment = ifelse(str_detect(trait_df_cl$trait_name, "\\(specific leaf area(.)*")  == TRUE, #removing the big parentheses from trait_name in SLA and pasting in comment
+                                                paste(comment, "|" , str_extract(trait_df_cl$trait_name, "\\(specific leaf area(.)*")), 
+                                                comment),
+                               comment = str_remove(comment, "x"), #cleaning up messy formating
+                               comment = paste0(comment, ". Value Kind Name = ", value_kind_name, "| TRY TraitID = ", trait_id), #storying the measurement kind (i.e. mean, best estimate) in the comment
+                               comment = ifelse(str_detect(trait_df_cl$trait_name, "Root length (.)*")  == TRUE, #cleaning up trait name and putting in comment
+                                                paste(comment, str_extract(trait_df_cl$trait_name, "Root length (.)*")), 
+                                                comment),
+                               trait_name = fct_recode(trait_name, 
+                                                       "Specific root length" = "Root length per root dry mass (specific root length, SRL)")) |> #renaming long name to SRL
+                        select(-c(dataset_id, observation_id, data_id, dataset, trait_id, value_kind_name)) |> 
+                        relocate(spp_name, trait_name, value, unit_name, source, id_dfdo, comment) 
+
+write.csv(cleaned_trait_df, "~/maia1/Data/Trait/TRY_angio_trait.csv")
 
 #General notes--------------------------------
-#   - 28 different data ID/traits
+
 #   - 2311 duplicates
 #   - Leaf area index (LAI) of a single plant was filtered out as it only has one observation
-#   - next to do is figure out the mismatches***
+#   - next to do is figure out the mismatches; ok. see line 134.
 
 
 
