31284ab1376265a34b5e5d1c65f2a35741cb86c6
Anh Mai <maimac@infra04-wg324.eduroam.wireless.umn.edu>
2026-09-14 10:19:56 -0500

    joining species lists and creating table

--- 260914_Species_list_merge/260904_Species_merge.R
+++ 260914_Species_list_merge/260904_Species_merge.R
@@ -2,5 +2,53 @@
 
 library(dplyr)
 
-REF_SPECIES <- read.csv("Documents/Data/FIADB_REFERENCE/REF_SPECIES.csv") |> select(SCIENTIFIC_NAME)
-write.csv(REF_SPECIES, "~/maia1/260914_Species_list_merge/fia_spp_ls.csv")
+#Libraries--------------------------------------------------------------------------------
+lib_ls <- c("dplyr",
+            "ggplot2",
+            "forcats",
+            "janitor",
+            "stringr",
+            "readxl")
+
+sapply(lib_ls, library, character.only = TRUE)
+
+#Data--------------------------------------------------------------------------------
+data_dir <- "~/maia1/Data/"
+
+#FIA
+fia_spp <- read.csv(paste0(data_dir, "FIA/c_REF_SPECIES.csv")) |> select(SCIENTIFIC_NAME)
+
+#Morton
+morton_check_ls <- read.csv(paste0(data_dir, "Morton/The Checklist of U.S. Trees - Checklist of U.S. Trees.csv")) |> select(Species.Name)
+
+#Interview
+interview_ls <- read.delim("~/maia1/260824_info_interview_analysis/spp_tab.txt", sep = " ")
+
+#Inner-join of FIA and Morton--------------------------------------------------------------------------------
+fia_morton <- fia_spp |>
+                  inner_join(morton_check_ls, by = join_by("SCIENTIFIC_NAME" == "Species.Name")) |>
+                  clean_names() |>
+                  mutate(genus = str_extract(scientific_name, "[A-z]*"),
+                         InterviewMN_Y= ifelse(genus %in% interview_ls$genus == "TRUE", "Y", "N"))
+
+
+#Overview of FIA and Morton--------------------------------------------------------------------------------
+
+#number of species that is identified by morton that is in FIA
+n_fia_coverage <- dim(fia_morton)[1] 
+n_fia_coverage
+
+#percent coverage of morton by FIA
+percent_coverage <- n_fia_coverage/dim(morton_check_ls)[1] 
+percent_coverage
+
+fia_morton |>
+  group_by(genus) |>
+  count(InterviewMN_Y) |>
+  filter(InterviewMN_Y == "Y" | n > 9) |>
+  arrange(desc(n)) |>
+  mutate(Mention = ifelse(InterviewMN_Y == "Y", "*", " ")) |>
+  select(-InterviewMN_Y) |>
+  relocate(genus, n) |>
+  rename(n_spp = n) |>
+  knitr::kable()
